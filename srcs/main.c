0f0240d371cac662c6a54194d4248c08f9ef90cd
cdurdetrouver <gabriel.bazart@gmail.com>
2024-08-03 17:29:23 +0200

    tets actions

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gbazart <gbazart@student.42.fr>            +#+  +:+       +#+        */
+/*   By: gbazart <gabriel.bazart@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 14:45:25 by gbazart           #+#    #+#             */
-/*   Updated: 2024/07/27 19:34:02 by gbazart          ###   ########.fr       */
+/*   Updated: 2024/08/03 17:29:12 by gbazart          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int	main(int argc, char **argv)
 		ft_fprintf(2, "Too much map\nNeed less than 10 maps\n");
 		return (EXIT_FAILURE);
 	}
+	ft_printf("Welcome to so_long\n");
 	check_argv(argv);
 	init();
 	mlx_key_hook(mlx()->win, key_hook, NULL);
